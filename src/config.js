249397aaa6bbea3193cc83039ead263947c9e0a2
KalebNiven <kaleb.niven7@gmail.com>
2023-10-04 19:30:45 +0500

    removed localhost based keys

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-    clientId: "807847415697-3erg2gu1dfh0lutqldkm1te2b485ff45.apps.googleusercontent.com",
-    developerKey: "AIzaSyDKDkRm2nNdzNtpe5CovXTYjf5dqWo4hRQ",
-    apiURL: 'http://localhost:5000'
-}
